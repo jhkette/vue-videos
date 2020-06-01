835ef4f315ddf35d8ddcd86c64c4bfc8bc3d953d
khenryjoe <joseph.ketterer@gmail.com>
2020-06-01 18:52:31 +0100

    a

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import qs from 'qs'
-const CLIENT_ID = '568995abdb5f6f8'
-const ROOT_URL = 'https://api.imgur.com/'
+import{ CLIENT_ID, ROOT_URL } from '../config/config'
+
 
 
 export default {
--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,4 @@
-const clientID = '568995abdb5f6f8'
-
-const secret = '8b315f7e36bf5cecab8689d2df707daae23b8248'
-
-const URL = 'https://api.imgur.com/oauth2/authorize?client_id=568995abdb5f6f8&response_type=REQUESTED_RESPONSE_TYPE&state=APPLICATION_STATE'
-
-export {clientID, secret}
+const CLIENT_ID = '568995abdb5f6f8'
+const ROOT_URL = 'https://api.imgur.com/'
+export {CLIENT_ID , ROOT_URL}
 
